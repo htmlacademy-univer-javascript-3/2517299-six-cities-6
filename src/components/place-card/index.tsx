3a152ead338964716d276747c205b56431a944e2
ka.vladykina <ka.vladykina@tbank.ru>
2025-10-26 15:26:42 +0300

    feat(module4-task1): 4.19. Обратная сторона реальности

--- src/components/place-card/index.tsx
+++ src/components/place-card/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import { Offer } from '../../types/offers';
+import { Link } from 'react-router-dom';
 
-const PlaceCard: React.FC = () => (
-  <article className="cities__card place-card">
+type PlaceCardProps = {
+  offer: Offer;
+  onHover?: (id: string | null) => void;
+};
+
+const PlaceCard: React.FC<PlaceCardProps> = ({ offer, onHover }) => (
+  <article
+    className="cities__card place-card"
+    onMouseEnter={() => onHover && onHover(offer.id)}
+    onMouseLeave={() => onHover && onHover(null)}
+  >
+    {offer.isPremium && (
+      <div className="place-card__mark">
+        <span>Premium</span>
+      </div>
+    )}
     <div className="cities__image-wrapper place-card__image-wrapper">
-      <a href="#">
+      <Link to={`/offer/${offer.id}`}>
         <img
           className="place-card__image"
-          src="img/room.jpg"
+          src={offer.previewImage}
           width="260"
           height="200"
           alt="Place image"
         />
-      </a>
+      </Link>
     </div>
     <div className="place-card__info">
       <div className="place-card__price-wrapper">
@@ -20,13 +36,17 @@ const PlaceCard: React.FC = () => (
           <span className="place-card__price-text">&#47;&nbsp;night</span>
         </div>
         <button
-          className="place-card__bookmark-button place-card__bookmark-button--active button"
+          className={`place-card__bookmark-button button ${
+            offer.isFavorite ? 'place-card__bookmark-button--active' : ''
+          }`}
           type="button"
         >
           <svg className="place-card__bookmark-icon" width="18" height="19">
             <use xlinkHref="#icon-bookmark"></use>
           </svg>
-          <span className="visually-hidden">In bookmarks</span>
+          <span className="visually-hidden">
+            {offer.isFavorite ? 'In bookmarks' : 'To bookmarks'}
+          </span>
         </button>
       </div>
       <div className="place-card__rating rating">
@@ -36,9 +56,9 @@ const PlaceCard: React.FC = () => (
         </div>
       </div>
       <h2 className="place-card__name">
-        <a href="#">Wood and stone place</a>
+        <a href="#">{offer.title}</a>
       </h2>
-      <p className="place-card__type">Room</p>
+      <p className="place-card__type">{offer.type}</p>
     </div>
   </article>
 );
